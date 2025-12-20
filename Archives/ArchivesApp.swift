b27e87a2dc0cf319f1d2fd325df674cc671a820b
Alexey Novikov <v@2dubs.com>
2025-12-20 11:10:45 -0500

    Initial Commit

---
+++ Archives/ArchivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchivesApp.swift
+//  Archives
+//
+//  Created by velocityzen on 2025-12-20.
+//
+
+import SwiftUI
+
+@main
+struct ArchivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArchivesTests/ArchivesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArchivesTests.swift
+//  ArchivesTests
+//
+//  Created by velocityzen on 2025-12-20.
+//
+
+import Testing
+@testable import Archives
+
+struct ArchivesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
